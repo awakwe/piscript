d5e6a9c2724b6bc8cc19258c7c87943e8378d2a7
U. Anthony Omegbu <anthonyomegbu@gmail.com>
2022-09-05 13:15:56 -0500

    Update dualscreen.sh

--- dualscreen.sh
+++ dualscreen.sh
@@ -1,31 +1,8 @@
 #!/bin/bash
 
 # Define the filename
-filename='/boot/config.txt'
+destination='/boot/config.txt'
 
-# Type the text that you want to append
-read -p "hdmi_edid_file:1=1" newtext1
-
-# Check the new text is empty or not
-if [ "$newtext1" != "" ]; then
-      # Append the text by using '>>' symbol
-      echo $newtext1 >> $filename
-fi
-
-# Type the text that you want to append
-read -p "hdmi_edid_filename:1=edid.dat" newtext2
-
-# Check the new text is empty or not
-if [ "$newtext2" != "" ]; then
-      # Append the text by using '>>' symbol
-      echo $newtext2 >> $filename
-fi
-
-# Type the text that you want to append
-read -p "hdmi_force_hotplug:1=1" newtext3
-
-# Check the new text is empty or not
-if [ "$newtext3" != "" ]; then
-      # Append the text by using '>>' symbol
-      echo $newtext3 >> $filename
-fi
+echo "hdmi_edid_file:1=1" >> $destination
+echo "hdmi_edid_filename:1=edid.dat" >> $destination
+echo "hdmi_force_hotplug:1=1" >> $destination
