7499346a9855f8cb90b473d1dacf95e6068ee64d
U. Anthony Omegbu <anthonyomegbu@gmail.com>
2022-09-15 13:22:09 -0500

    Update Wordpress-lamp.sh

--- Wordpress-lamp.sh
+++ Wordpress-lamp.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt update && sudo apt upgrade -y
-sudo apt install apache2 php libapache2-mod-php mysql-server php-mysql -y
+sudo apt install apache2 php libapache2-mod-php php-mysql -y
+sudo apt-get install default-mysql-server
 sudo systemctl start mysql.service
 sudo service apache2 restart
 cd /var/www/html/
@@ -10,10 +11,11 @@ sudo tar xzf latest.tar.gz
 sudo mv wordpress/* .
 sudo rm -rf wordpress latest.tar.gz
 sudo chown -R www-data: .
+cd
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar 
 sudo mv wp-cli.phar /usr/local/bin/wp
-mysql -u root -e "CREATE DATABASE KasaDatabase";
+sudo mysql -u root -e "CREATE DATABASE KasaDatabase";
 Sudo mysql -u root -p "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'kasatech1010!'";
 sudo mysql_secure_installation
 sudo reboot
