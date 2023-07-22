d7c140d153305a162e083c1ba4b98f7d860e6528
U. Anthony Omegbu <anthonyomegbu@gmail.com>
2023-07-21 23:59:49 -0500

    Create install_jellyfin.sh

---
+++ install_jellyfin.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Updating Termux repo
+pkg update
+
+# Installing necessary packages
+pkg install proot-distro ffmpeg -y
+
+# Installing Ubuntu
+proot-distro install ubuntu
+
+# Logging into Ubuntu
+proot-distro login ubuntu
+
+# Updating and upgrading the packages in Ubuntu
+apt update && apt upgrade -y
+
+# Installing necessary packages
+apt install sudo curl gnupg -y
+
+# Step 3: Download the GPG signing key
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://repo.jellyfin.org/jellyfin_team.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/jellyfin.gpg
+
+# Step 4: Add a repository configuration
+cat <<EOF | sudo tee /etc/apt/sources.list.d/jellyfin.sources
+Types: deb
+URIs: https://repo.jellyfin.org/$( awk -F'=' '/^ID=/{ print $NF }' /etc/os-release )
+Suites: $( awk -F'=' '/^VERSION_CODENAME=/{ print $NF }' /etc/os-release )
+Components: main
+Architectures: $( dpkg --print-architecture )
+Signed-By: /etc/apt/keyrings/jellyfin.gpg
+EOF
+
+# Step 5: Update APT repositories
+sudo apt update
+
+# Step 6: Install Jellyfin
+sudo apt install jellyfin
+
+# Creating a symbolic link for Jellyfin web client
+ln -s /usr/share/jellyfin/web /usr/lib/jellyfin/bin/jellyfin-web
+
+# Running Jellyfin
+jellyfin
